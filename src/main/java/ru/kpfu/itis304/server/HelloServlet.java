e71fe2fc8eb79fe4ed195601ad5e20598ded5ddf
makarjaan <arina.makarova.08@inbox.ru>
2024-11-02 22:54:59 +0300

    Homework Client-server

---
+++ src/main/java/ru/kpfu/itis304/server/HelloServlet.java
@@ -0,0 +1,55 @@
+package ru.kpfu.itis304.server;
+
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.stream.Collectors;
+
+@WebServlet("/")
+public class HelloServlet extends HttpServlet {
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String requestBody = req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        PrintWriter writer = resp.getWriter();
+        writer.print("POST" + requestBody);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        String idParam = req.getParameter("id");
+        if (idParam == null) {
+            writer.print("GET ERROR");
+            return;
+        }
+        writer.print("GET");
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        String requestBody = req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        PrintWriter writer= resp.getWriter();
+        writer.print("PUT" + requestBody);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        String idParam = req.getParameter("id");
+        if (idParam == null) {
+            writer.print("DELETE ERROR");
+            return;
+        }
+        writer.print("DELETE");
+    }
+}
+
---
+++ src/main/java/ru/kpfu/itis304/server/Main.java
@@ -0,0 +1,48 @@
+package ru.kpfu.itis304.server;
+
+import ru.kpfu.itis304.les1.Client;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Client client = new Client();
+
+        Map<String, String> headers =  new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        headers.put("Accept", "application/json");
+        headers.put("Authorization", "Bearer 58762cdab4e248c10d165f6bbe89d18a444dff00267b6cfcec49acf9dceb94b7");
+
+        Map<String, String> postData = new HashMap<>();
+        postData.put("id", "1");
+        postData.put("title", "welcome");
+        postData.put("body", "hello world");
+        System.out.println(client.post("http://localhost:8080/ORIS_war/", headers, postData));
+
+
+        Map<String, String> postData2 = new HashMap<>();
+        postData2.put("id", "2");
+        postData2.put("title", "way");
+        postData2.put("body", "kruto");
+        System.out.println(client.post("http://localhost:8080/ORIS_war/", headers, postData2));
+
+
+        Map<String, String> getData = new HashMap<>();
+        getData.put("id", "1");
+        System.out.println(client.get("http://localhost:8080/ORIS_war/", headers, getData));
+
+
+        Map<String, String> putData = new HashMap<>();
+        putData.put("title", "srabotalo");
+        putData.put("body", "yra!");
+        System.out.println(client.put("http://localhost:8080/ORIS_war/?id=2", headers, putData));
+
+
+        Map<String, String> deleteHeaders =  new HashMap<>();
+        System.out.println(client.delete("http://localhost:8080/ORIS_war/?id=1", deleteHeaders, null));
+
+
+
+    }
+}
